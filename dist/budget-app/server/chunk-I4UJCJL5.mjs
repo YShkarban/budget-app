8f5e05b40a84822cd638034fc135efb202b732b1
yaroslav.shkarban <yarik.shkarban@gmail.com>
2025-09-28 22:06:33 +0200

    build

--- dist/budget-app/server/chunk-I4UJCJL5.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import"./chunk-O4DHGN6X.mjs";import"./chunk-KB5IWD3E.mjs";import"./chunk-H6KHSOBK.mjs";
--- dist/budget-app/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-ZDONJZXY.mjs";import"./chunk-S3MD6RQN.mjs";import"./chunk-BJHCEIPM.mjs";import"./chunk-O4DHGN6X.mjs";import"./chunk-KB5IWD3E.mjs";import"./chunk-H6KHSOBK.mjs";export{a as default};
